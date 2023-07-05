b29fc55461425e25bca3cc419414da9fe3b7383c
Jasper Van Dyck <jasper.vandyck@hotmail.be>
2023-07-05 17:21:21 +0200

    refactored modal

--- src/components/calendar/Modal.tsx
+++ src/components/calendar/Modal.tsx
@@ -1,54 +1,20 @@
-import { UseMutateAsyncFunction } from "@tanstack/react-query";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { ArrowsRightLeftIcon } from "@heroicons/react/24/outline";
+import TrainingModal from "./TrainingModal";
+import { useState } from "react";
+import CompetitionModal from "./CompetitionModal";
+import useSaveEvent from "../../hooks/useSaveEvent";
+import useSaveCompetition from "../../hooks/useSaveCompetition";
 
-interface EventInput {
-  title: string;
-  start: string;
-  end: string;
+interface ModalProps {
+  closeModal: () => void;
+  day: Date;
 }
 
-export const Modal = ({
-  closeModal,
-  mutateAsync,
-  day,
-}: {
-  closeModal: () => void;
-  mutateAsync: UseMutateAsyncFunction<BasicEvent, unknown, SaveEvent, unknown>;
-  day: Date;
-}) => {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<EventInput>();
+export const Modal = ({ closeModal, day }: ModalProps) => {
+  const [event, setEvent] = useState("Training");
 
-  const options = [
-    { value: "Schermschool", label: "Schermschool" },
-    { value: "Volwassenen & Beloften", label: "Volwassenen & Beloften" },
-    { value: "Clubbeker", label: "Clubbeker" },
-  ];
-
-  const onSubmit: SubmitHandler<EventInput> = (data) => {
-    const start = new Date(day);
-
-    start.setHours(Number(data.start.split(":")[0]));
-    start.setMinutes(Number(data.start.split(":")[1]));
-    console.log(start);
-
-    const end = new Date(day);
-    end.setHours(Number(data.end.split(":")[0]));
-    end.setMinutes(Number(data.end.split(":")[1]));
-
-    console.log(end);
-
-    mutateAsync({
-      ...data,
-      id: undefined,
-      start: start,
-      end: end,
-    });
-    closeModal();
-  };
+  const { mutateAsync: mutateEvent } = useSaveEvent();
+  const { mutateAsync: mutateCompetition } = useSaveCompetition();
 
   return (
     <>
@@ -56,80 +22,30 @@ export const Modal = ({
         <div className='flex min-h-screen items-center justify-center'>
           <div className='transform overflow-hidden rounded-lg bg-white transition-all sm:w-full sm:max-w-lg'>
             <div className='p-6'>
-              <div className='mb-1 text-lg font-medium text-gray-900'>
-                Nieuwe training inplannen
-              </div>
-              <div className='mb-4 text-sm text-gray-500'>
-                Vul onderstaande velden correct in.
-              </div>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                  <label>Titel</label>
-                  <select
-                    placeholder='Titel'
-                    {...register("title", {
-                      required: "Titel is verplicht",
-                    })}
-                    aria-invalid={errors?.title ? "true" : "false"}
-                  >
-                    <option value='Schermschool'>Schermschool</option>
-                    <option value='Volwassenen & Beloften'>
-                      Volwassenen & Beloften
-                    </option>
-                    <option value='Clubbeker'>Clubbeker</option>
-                    {options.map((o) => (
-                      <option key={o.value} value={o.value}>
-                        {o.label}
-                      </option>
-                    ))}
-                  </select>
-                  <p className='h-5 text-sm text-gray-400 empty:invisible'>
-                    {errors?.title?.message}
-                  </p>
-                </div>
-                <div className='flex w-full justify-between gap-4'>
-                  <div className='w-full'>
-                    <label>Start</label>
-                    <input
-                      type='time'
-                      placeholder='Start'
-                      {...register("start", {
-                        required: "Start is verplicht",
-                      })}
-                      aria-invalid={errors?.start ? "true" : "false"}
-                    />
-                    <p className='h-5 text-sm text-gray-400 empty:invisible'>
-                      {errors?.start?.message}
-                    </p>
-                  </div>
-                  <div className='w-full'>
-                    <label>Einde</label>
-                    <input
-                      type='time'
-                      placeholder='Einde'
-                      {...register("end", {
-                        required: "Einde is verplicht",
-                      })}
-                      aria-invalid={errors?.end ? "true" : "false"}
-                    />
-                    <p className='h-5 text-sm text-gray-400 empty:invisible'>
-                      {errors?.end?.message}
-                    </p>
-                  </div>
-                </div>
-
-                <div className='mt-4 flex justify-end gap-6'>
-                  <button
-                    onClick={closeModal}
-                    className='inline-flex w-full rounded-md py-2 text-base font-medium text-gray-400 hover:text-gray-600 sm:ml-0 sm:w-auto sm:text-sm'
-                  >
-                    Annuleer
-                  </button>
-                  <button className='btn-primary' type='submit'>
-                    Verifieer
-                  </button>
-                </div>
-              </form>
+              <button
+                className='0 absolute right-10 z-10 flex items-center gap-2 rounded-md border bg-gray-200 px-3 py-2 text-black shadow-md hover:bg-gray-300'
+                onClick={() =>
+                  event === "Training"
+                    ? setEvent("Wedstrijd")
+                    : setEvent("Training")
+                }
+              >
+                <ArrowsRightLeftIcon className='h-4 w-4 object-contain' />
+                <span>{event === "Training" ? "Wedstrijd" : "Training"}</span>
+              </button>
+              {event === "Training" ? (
+                <TrainingModal
+                  closeModal={closeModal}
+                  mutateAsync={mutateEvent}
+                  day={day}
+                />
+              ) : (
+                <CompetitionModal
+                  closeModal={closeModal}
+                  mutateAsync={mutateCompetition}
+                  day={day}
+                />
+              )}
             </div>
           </div>
         </div>
